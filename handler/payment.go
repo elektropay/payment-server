7b4ea2b6ecb7fb961f4162bc7d695ca05d37ace8
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2019-03-21 18:21:24 +0000

    Decoding request

--- handler/payment.go
+++ handler/payment.go
@@ -1,30 +1,60 @@
 package handler
 
 import (
+	"encoding/json"
+	"github.com/teivah/payment-server/swagger"
+	"github.com/teivah/payment-server/utils"
+	"go.uber.org/zap"
 	"net/http"
 )
 
-func PaymentIdDelete(w http.ResponseWriter, r *http.Request) {
+func PaymentIdDelete(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 }
 
-func PaymentIdGet(w http.ResponseWriter, r *http.Request) {
+func PaymentIdGet(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 }
 
-func PaymentIdPut(w http.ResponseWriter, r *http.Request) {
+func PaymentIdPut(w http.ResponseWriter, request *http.Request) {
+	var payment swagger.PaymentUpdate
+	err := decodeRequest(payment, request)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 }
 
-func PaymentsGet(w http.ResponseWriter, r *http.Request) {
+func PaymentsGet(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 }
 
-func PaymentsPost(w http.ResponseWriter, r *http.Request) {
+func PaymentsPost(w http.ResponseWriter, request *http.Request) {
+	var payment swagger.PaymentCreation
+	err := decodeRequest(payment, request)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+}
+
+func decodeRequest(v interface{}, request *http.Request) error {
+	decoder := json.NewDecoder(request.Body)
+
+	err := decoder.Decode(&v)
+	if err != nil {
+		utils.Logger.Error("Error while decoding request", zap.Error(err))
+		return err
+	}
+
+	return nil
 }
